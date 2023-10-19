1f1df855f98e8667de6c21e2c928a1c9edeae125
blacksg <sagwa.nth@gmail.com>
2023-10-19 11:50:39 +0900

    init: 백엔드 프로젝트 디렉터리 생성 및 의존성 추가

---
+++ ecoders-backend/src/main/java/ecoders/polareco/PolarecoApplication.java
@@ -0,0 +1,12 @@
+package ecoders.polareco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolarecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolarecoApplication.class, args);
+	}
+}
