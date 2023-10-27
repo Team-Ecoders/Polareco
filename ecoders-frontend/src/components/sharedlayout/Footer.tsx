b83bc34543143df60cfdd5ed8da0f9a927e15521
strong-minsu <dlalstn5789@gmail.com>
2023-10-28 00:50:02 +0900

    style: footer 반응형 스타일

--- ecoders-frontend/src/components/sharedlayout/Footer.tsx
+++ ecoders-frontend/src/components/sharedlayout/Footer.tsx
@@ -10,48 +10,53 @@ function Footer() {
             <FooterLogoTitle>
               <FooterLogo src={Logo} />
               POLARECO
+              <FooterLogoContent2>
+                건강한 지구, 건강한 라이프 건강한 가치를 추구하는 에코 플랫폼 폴라레코입니다.
+              </FooterLogoContent2>
             </FooterLogoTitle>
-            <FooterLogoContent>
+            <FooterLogoContent1>
               건강한 지구, 건강한 라이프 <br />
               건강한 가치를 추구하는 에코 플랫폼 <br />
               폴라레코입니다.
-            </FooterLogoContent>
+            </FooterLogoContent1>
           </FooterSectionContainer>
-          <FooterSectionContainer>
-            <FooterSectionTitle>Company</FooterSectionTitle>
-            <FooterSectionContent>
-              <div className="footer-content">About Us</div>
-              <div className="footer-content">Contact Us</div>
-              <div className="footer-content">Features</div>
-            </FooterSectionContent>
-          </FooterSectionContainer>
-          <FooterSectionContainer>
-            <FooterSectionTitle>Services</FooterSectionTitle>
-            <FooterSectionContent>
-              <div className="footer-content">Eco-Habit</div>
-              <div className="footer-content">Challenge</div>
-              <div className="footer-content">Community</div>
-              <div className="footer-content">Sponsor</div>
-            </FooterSectionContent>
-          </FooterSectionContainer>
-          <FooterSectionContainer>
-            <FooterSectionTitle>Contact</FooterSectionTitle>
-            <FooterSectionContent>
-              <div className="footer-content">(+82) 10-0000-0000</div>
-              <div className="footer-content">polareco.official@gmail.com</div>
-              <div className="footer-content">
-                Address: Gangnamgu, <div>Seoul, 03232</div>
-              </div>
-            </FooterSectionContent>
-          </FooterSectionContainer>
-          <FooterSectionContainer>
-            <FooterSectionTitle>Link</FooterSectionTitle>
-            <FooterSectionContent>
-              <div className="footer-content">Terms of Service</div>
-              <div className="footer-content">Privacy Policy</div>
-              <div className="footer-content">Career</div>
-            </FooterSectionContent>
-          </FooterSectionContainer>
+          <FooterContentsContainer>
+            <FooterSectionContainer>
+              <FooterSectionTitle>Company</FooterSectionTitle>
+              <FooterSectionContent>
+                <div className="footer-content">About Us</div>
+                <div className="footer-content">Contact Us</div>
+                <div className="footer-content">Features</div>
+              </FooterSectionContent>
+            </FooterSectionContainer>
+            <FooterSectionContainer>
+              <FooterSectionTitle>Services</FooterSectionTitle>
+              <FooterSectionContent>
+                <div className="footer-content">Eco-Habit</div>
+                <div className="footer-content">Challenge</div>
+                <div className="footer-content">Community</div>
+                <div className="footer-content">Sponsor</div>
+              </FooterSectionContent>
+            </FooterSectionContainer>
+            <FooterSectionContainer>
+              <FooterSectionTitle>Contact</FooterSectionTitle>
+              <FooterSectionContent>
+                <div className="footer-content">(+82) 10-0000-0000</div>
+                <div className="footer-content">polareco.official@gmail.com</div>
+                <div className="footer-content">
+                  Address: Gangnamgu, <div>Seoul, 03232</div>
+                </div>
+              </FooterSectionContent>
+            </FooterSectionContainer>
+            <FooterSectionContainer>
+              <FooterSectionTitle>Link</FooterSectionTitle>
+              <FooterSectionContent>
+                <div className="footer-content">Terms of Service</div>
+                <div className="footer-content">Privacy Policy</div>
+                <div className="footer-content">Career</div>
+              </FooterSectionContent>
+            </FooterSectionContainer>
+          </FooterContentsContainer>
         </FooterInfoContainer>
         <FooterLine />
         <RigthInfo>© 2023 Polareco - All Rights Reserved</RigthInfo>
@@ -68,6 +73,7 @@ const Entire = styled.div`
   align-items: center;
   z-index: 40;
   background-color: #131313;
+  margin-top: 80px;
 `;
 
 const FooterLogo = styled.img`
@@ -80,38 +86,39 @@ const FooterContainer = styled.div`
   width: 100%;
   max-width: 1520px;
   margin: 0 auto;
-  height: 400px;
+  /* height: 400px; */
   display: flex;
   flex-direction: column;
   align-items: center;
   bottom: 0;
-
-  @media (max-width: 1152px) {
-    // 화면 크기가 768px 이하일 때
-    /* transform: scale(0.6); // 이 줄을 추가 */
-  }
-
-  @media (max-width: 768px) {
-    // 화면 크기가 768px 이하일 때
-    /* transform: scale(0.4); // 이 줄을 추가 */
-  }
-
-  @media (max-width: 480px) {
-    // 화면 크기가 480px 이하일 때
-    /* transform: scale(0.25); // 이 줄을 추가 */
-  }
 `;
 
 const FooterInfoContainer = styled.div`
   display: flex;
   justify-content: space-around;
   width: 100%;
+  flex-wrap: wrap;
 `;
 
 const FooterSectionContainer = styled.div`
   margin-top: 40px;
 `;
 
+const FooterContentsContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  width: 60%;
+  flex-wrap: wrap;
+  @media (max-width: 768px) {
+    // 화면 크기가 768px 이하일 때
+    width: 100%;
+  }
+  @media (max-width: 480px) {
+    width: 60%;
+    display: block;
+    justify-content: left;
+  }
+`;
 const FooterSectionTitle = styled.div`
   font-size: 25px;
   color: var(--White, #fdfdfd);
@@ -122,10 +129,21 @@ const FooterSectionTitle = styled.div`
   font-weight: 600;
   line-height: 130%; /* 46.8px */
   margin-bottom: 40px;
+  @media (max-width: 768px) {
+    // 화면 크기가 768px 이하일 때
+    font-size: 18px;
+    font-weight: 600;
+    line-height: 120%;
+    margin-bottom: 20px;
+  }
 `;
 
 const FooterLogoTitle = styled(FooterSectionTitle)`
   font-weight: 900;
+  @media (max-width: 768px) {
+    // 화면 크기가 768px 이하일 때
+    font-weight: 600;
+  }
 `;
 
 const FooterSectionContent = styled.div`
@@ -142,9 +160,17 @@ const FooterSectionContent = styled.div`
     margin-bottom: 20px;
     white-space: nowrap;
   }
+
+  @media (max-width: 768px) {
+    // 화면 크기가 768px 이하일 때
+    font-size: 14px;
+    text-align: left;
+    font-style: normal;
+    font-weight: 600;
+  }
 `;
 
-const FooterLogoContent = styled(FooterSectionContent)`
+const FooterLogoContent1 = styled(FooterSectionContent)`
   color: #b2afaf;
 
   font-family: 'Inter';
@@ -153,6 +179,31 @@ const FooterLogoContent = styled(FooterSectionContent)`
   font-weight: 400;
   line-height: 160%;
   width: 304px;
+
+  @media (max-width: 768px) {
+    display: none;
+  }
+  @media (max-width: 480px) {
+    display: block;
+  }
+`;
+
+const FooterLogoContent2 = styled(FooterSectionContent)`
+  color: #b2afaf;
+  display: none;
+  font-family: 'Inter';
+  font-size: 14px;
+  margin-left: 5px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 160%;
+  width: 304px;
+  @media (max-width: 768px) {
+    display: inline;
+  }
+  @media (max-width: 480px) {
+    display: none;
+  }
 `;
 
 const FooterLine = styled.hr`
@@ -175,4 +226,5 @@ const RigthInfo = styled.div`
   font-weight: 600;
   line-height: 130%;
   margin-top: 30px;
+  margin-bottom: 30px;
 `;
