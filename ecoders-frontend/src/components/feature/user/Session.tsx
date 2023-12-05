793a7ca24731f7dcb553a03ee82bc424f86a2f0f
strong-minsu <dlalstn5789@gmail.com>
2023-12-05 14:13:01 +0900

    refreshToken 요청 함수 생성

--- ecoders-frontend/src/components/feature/user/Session.tsx
+++ ecoders-frontend/src/components/feature/user/Session.tsx
@@ -8,17 +8,63 @@ import { setEmail, setId, setUsername, setProfileImg } from '../../../redux/slic
 //vite로 만든 프로젝트에서 환경변수 사용하기
 const APIURL = import.meta.env.VITE_API_URL;
 
-function Session({ setIsLoading }: { setIsLoading: React.Dispatch<React.SetStateAction<boolean>> }) {
+export async function tokenExpirationHandler(fun: Function) {
+  const dispatch = useDispatch();
+  const refreshToken = localStorage.getItem('refreshToken');
+
+  try {
+    // refreshToken으로 accessToken 재발급 시도
+    const response = await axios.get(`${APIURL}/token/reissue`, {
+      headers: {
+        'Refresh-Token': refreshToken,
+        //ngrok 사용시에만 넣음
+        'ngrok-skip-browser-warning': 'skip-browser-warning',
+      },
+    });
+
+    //refreshToken이 유효한 상태 accessToken 재등록
+    if (response.status === 200) {
+      //accessToken 재등록
+      const newAccessToken = response.data['authorization'];
+      const newRefreshToken = response.data['refreshToken'];
+      localStorage.setItem('accessToken', newAccessToken);
+      localStorage.setItem('refreshToken', newRefreshToken);
+
+      // accessToken 재등록 후 callBack 함수 호출
+      fun();
+    }
+  } catch (err: any) {
+    //refreshToken도 만료됨
+    if (err.resopnse.status === 403) {
+      // 로컬 스토리지 토큰들 삭제
+      localStorage.removeItem('accessToken');
+      localStorage.removeItem('refreshToken');
+
+      alert('로그인을 다시 시도해주세요');
+
+      //로그아웃
+      dispatch(logout());
+      // setIsLoading(false);
+    } else {
+      console.log(err);
+      // setIsLoading(false);
+    }
+  }
+}
+
+function Session() {
+  // { setIsLoading }: { setIsLoading: React.Dispatch<React.SetStateAction<boolean>> }
   const dispatch = useDispatch();
   //로그인 상태 받아옴
   const isLoggedIn = useSelector((state: RootState) => state.login.isLoggedIn);
 
+  console.log('session');
   //유저 정보 받아오는 함수
   async function getUser() {
     const accessToken = localStorage.getItem('accessToken');
-    const refreshToken = localStorage.getItem('refreshToken');
+
     try {
-      setIsLoading(true);
+      // setIsLoading(true);
       const response = await axios.get(`${APIURL}/member/my-info`, {
         headers: {
           Authorization: accessToken,
@@ -35,47 +81,12 @@ function Session({ setIsLoading }: { setIsLoading: React.Dispatch<React.SetState
         //프로필 이미지
         // response.data.imageUrl && dispatch(setProfileImg(response.data.profileImage));
         console.log('User information has been received successfully.');
-        setIsLoading(false);
+        // setIsLoading(false);
       }
     } catch (err: any) {
       //accessToken 만료일 경우
       if (err.response.status === 403) {
-        try {
-          // refreshToken으로 accessToken 재발급 시도
-          const response = await axios.get(`${APIURL}/token/reissue`, {
-            headers: {
-              'Refresh-Token': refreshToken,
-              //ngrok 사용시에만 넣음
-              'ngrok-skip-browser-warning': 'skip-browser-warning',
-            },
-          });
-
-          //refreshToken이 유효한 상태 accessToken 재등록
-          if (response.status === 200) {
-            //accessToken 재등록
-            const newAccessToken = response.data['authorization'];
-            const newRefreshToken = response.data['refreshToken'];
-            localStorage.setItem('accessToken', newAccessToken);
-            localStorage.setItem('refreshToken', newRefreshToken);
-
-            // 재등록 후 유저 정보 불러오기
-            getUser();
-          }
-        } catch (err: any) {
-          //refreshToken도 만료됨
-          if (err.resopnse.status === 403) {
-            // 로컬 스토리지 토큰들 삭제
-            localStorage.removeItem('accessToken');
-            localStorage.removeItem('refreshToken');
-
-            //로그아웃
-            dispatch(logout());
-            setIsLoading(false);
-          } else {
-            console.log(err);
-            setIsLoading(false);
-          }
-        }
+        tokenExpirationHandler(getUser);
       }
     }
   }
@@ -87,7 +98,7 @@ function Session({ setIsLoading }: { setIsLoading: React.Dispatch<React.SetState
       getUser();
     } else {
       dispatch(logout());
-      setIsLoading(false);
+      // setIsLoading(false);
     }
     // // 로그인 상태일때 getUser 함수 호출
     // if (isLoggedIn) {
