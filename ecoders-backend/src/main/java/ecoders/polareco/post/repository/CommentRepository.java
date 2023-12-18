ed01f6a8e74ad2ca585da8d671896caf6d92480e
yang-jin-ho <yxino96@gmail.com>
2023-12-18 19:45:53 +0900

    feat: 커뮤니티 추가

---
+++ ecoders-backend/src/main/java/ecoders/polareco/post/repository/CommentRepository.java
@@ -0,0 +1,14 @@
+package ecoders.polareco.post.repository;
+
+import ecoders.polareco.post.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    @Query("SELECT c FROM Comment c WHERE c.post.postId = :postId")
+    List<Comment> findByPostId(@Param("postId") long postId);
+}
