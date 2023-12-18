6d070acaa09f1c1a20ef35fcbec3c739b95c2e76
strong-minsu <dlalstn5789@gmail.com>
2023-12-18 16:12:41 +0900

    feat: Homepage

--- ecoders-frontend/src/main.tsx
+++ ecoders-frontend/src/main.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
@@ -6,10 +6,10 @@ import { Provider } from 'react-redux';
 import store from './redux/store/store.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-    ,
-  </React.StrictMode>,
+  // <React.StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  // ,
+  // </React.StrictMode>,
 );
