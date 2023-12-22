dd752e7fd8cdeb77e2f87c5e26b9893bfe944a98
strong-minsu <dlalstn5789@gmail.com>
2023-12-22 15:47:45 +0900

    fix: 구글 로그인 수정, 로그인 시 user 정보 불러오기

--- ecoders-frontend/src/components/feature/user/Session.tsx
+++ ecoders-frontend/src/components/feature/user/Session.tsx
@@ -48,6 +48,7 @@ export async function tokenExpirationHandler(fun: Function) {
       //로그아웃
       // dispatch(logout());
       // setIsLoading(false);
+      window.location.reload();
     } else {
       console.log(err);
     }
@@ -77,8 +78,10 @@ function Session() {
         dispatch(setUsername(response.data.username));
         dispatch(setEmail(response.data.email));
         dispatch(setId(response.data.uuid));
-        dispatch(setProfileImg(response.data.profileImage));
-        //프로필 이미지
+        //이미지 있는 경우만 불러옴
+        if (response.data.profileImage) {
+          dispatch(setProfileImg(response.data.profileImage));
+        }
         console.log('User information has been received successfully.');
         dispatch(login());
       }
